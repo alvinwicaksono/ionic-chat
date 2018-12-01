4cf85344f8fb79790563ef097089fd5df73b69c3
alvinwicaksono <alvin.wicaksono@ti.ukdw.ac.id>
2018-12-01 21:27:20 +0700

    chat

---
+++ src/app/app.config.ts
@@ -0,0 +1,12 @@
+export const appconfig = {
+  firebase: {
+   apiKey: "AIzaSyAvGDU7j1ogZBytqfblzDSj5uY3-JGMN78",
+    authDomain: "ngomongo-f6265.firebaseapp.com",
+    databaseURL: "https://ngomongo-f6265.firebaseio.com",
+    projectId: "ngomongo-f6265",
+    storageBucket: "ngomongo-f6265.appspot.com",
+    messagingSenderId: "68674826732"
+  },
+  users_endpoint: "users",
+  chats_endpoint: "chats"
+};
